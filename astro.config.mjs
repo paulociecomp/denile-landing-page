c393e989d11af0e3cbff5ad83a1bad9a889aa6ad
Paulo Moura <paulo.moura@bhub.ai>
2025-04-11 12:44:04 -0300

    add deploy em github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  site: 'https://paulociecomp.github.io',
+  base: '/denile-landing-page',
+  integrations: [tailwind()]  
 });
